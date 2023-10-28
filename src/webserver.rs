eb49ce3d99dff486bca75d8744463b69641a3952
wizard <pranjalpatel9838@gmail.com>
2023-10-28 12:53:57 +0530

    add default route and read timeout

--- src/webserver.rs
+++ src/webserver.rs
@@ -1,18 +1,22 @@
+use std::collections::HashMap;
 use std::io::{Read, Write};
 use std::net::ToSocketAddrs;
 use std::net::{TcpListener, TcpStream};
+use std::time::Duration;
 
-use crate::{response, QueryParis};
+use crate::{response, Query};
 use response::Response;
 
 use scoped_threadpool::Pool;
 use url::Url;
 
-type Handler = dyn Fn(QueryParis) -> Response + 'static + Send + Sync;
+type Handler = dyn Fn(Query) -> Response + 'static + Send + Sync;
 
 pub struct WebServer {
     listener: TcpListener,
     routes: Vec<(String, Box<Handler>)>,
+    default_route: Option<Box<Handler>>,
+    read_timeout: Option<Duration>,
 }
 
 impl WebServer {
@@ -27,15 +31,38 @@ impl WebServer {
             },
 
             routes: Vec::new(),
+            default_route: None,
+            read_timeout: None,
         }
     }
 
-    pub fn route<F: Fn(QueryParis) -> Response + 'static + Send + Sync>(mut self, path: &str, func: F) -> Self {
+    pub fn read_timeout(mut self, timeout: Duration) -> Self {
+        self.read_timeout = Some(timeout);
+        self
+    }
+
+    pub fn get_addr(&self) -> std::io::Result<std::net::SocketAddr> {
+        self.listener.local_addr()
+    }
+
+    pub fn default_route<F: Fn(Query) -> Response + 'static + Send + Sync>(
+        mut self,
+        func: F,
+    ) -> Self {
+        self.default_route = Some(Box::new(func));
+        self
+    }
+
+    pub fn route<F: Fn(Query) -> Response + 'static + Send + Sync>(
+        mut self,
+        path: &str,
+        func: F,
+    ) -> Self {
         self.routes.push((path.to_string(), Box::new(func)));
         self
     }
 
-    pub fn start(self) -> ! {
+    pub fn start(&self) -> ! {
         let mut incoming = self.listener.incoming();
         let mut pool = Pool::new(crate::NUM_THREADS);
 
@@ -44,7 +71,7 @@ impl WebServer {
             let stream = stream.expect("Error handling TCP stream.");
 
             stream
-                .set_read_timeout(Some(std::time::Duration::from_secs(10)))
+                .set_read_timeout(self.read_timeout)
                 .expect("[Error] Couldn't set read timeout on socket");
 
             pool.scoped(|scope| {
@@ -54,6 +81,7 @@ impl WebServer {
     }
 
     fn handle_connection(&self, mut stream: TcpStream) {
+        #[cfg(debug)]
         println!("New connection: {}", stream.local_addr().unwrap());
 
         let mut buffer = [0; 1024];
@@ -65,19 +93,29 @@ impl WebServer {
         // Base url and scheme is not used here, that's why arbitrary url and scheme used
         let url = Url::parse(&format!("http://localhost{path}")).unwrap();
 
-        for (path, function) in self.routes.iter() {
-            if path == url.path() {
-                let query_pairs = url.query_pairs();
-                let response = function(
-                    query_pairs
-                        .into_iter()
-                        .map(|(key, value)| (key.into_owned(), value.into_owned()))
-                        .collect(),
-                );
+        let route_fn = self
+            .routes
+            .iter()
+            .find(|(p, _)| p == url.path())
+            .map(|(_, f)| f);
 
-                write!(stream, "{}", response).expect("Failed to write to stream");
-                stream.flush().expect("Failed to flush stream");
-            }
+        if route_fn.is_some() || self.default_route.is_some() {
+            let function = route_fn.unwrap_or_else(|| self.default_route.as_ref().unwrap());
+
+            let mut query_map: HashMap<String, Vec<String>> =
+                HashMap::with_capacity(url.query_pairs().count());
+            let query_pairs = url.query_pairs();
+            query_pairs.into_iter().for_each(|(k, v)| {
+                query_map
+                    .entry(k.into_owned())
+                    .or_default()
+                    .push(v.into_owned())
+            });
+
+            let response = function(query_map);
+
+            write!(stream, "{}", response).expect("Failed to write to stream");
+            stream.flush().expect("Failed to flush stream");
         }
     }
 
